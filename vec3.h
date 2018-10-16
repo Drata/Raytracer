01deee58a0bb3adec74dc99a95505b3a0ecf1509
DrAta <adescudero93@gmail.com>
2018-10-16 16:07:42 +0200

    vec3 class started

---
+++ vec3.h
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+class vec3 {
+  vec() {}
+  vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+  inline float x() const { return e[0]; }
+  inline float y() const { return e[1]; }
+  inline float z() const { return e[2]; }
+  inline float r() const { return e[0]; }
+  inline float g() const { return e[1]; }
+  inline float b() const { return e[2]; }
+
+  inline const vec3& operator+() const { return *this; }
+  inline vec3 operator-() const { return vec(-e[0], -e[1], -e[2]); }
+};
