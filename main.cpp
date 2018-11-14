39c52eb2fc6601e18305edae7d0dfc277560b1f4
DrAta <adescudero93@gmail.com>
2018-11-14 19:24:59 +0100

    Added hitable abstract class and children classes sphere and hitablelist

--- main.cpp
+++ main.cpp
@@ -1,46 +1,24 @@
 #include <iostream>
 #include "vec3.h"
 #include "ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
 
-float hit_sphere(const vec3& center, float radius, const ray &r) {
-  // vector from origin to center of sphere
-  vec3 oc = r.origin() - center;
-  float a = dot(r.direction(), r.direction());
-  float b = 2.0 * dot(oc, r.direction());
-  float c = dot(oc, oc) - radius * radius;
-  float discriminant = b * b - 4 * a * c;
+vec3 color(const ray& r, hitable *world) {
 
-  float t;
+  hit_record rec;
 
-  if (discriminant < 0) {
-    
-    return -1.0;
+  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+    return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
   } else {
-    // solution to the second grade ecuation that tell us where in the sphere
-    //    the ray is going to hit.
-    t = (-b - sqrt(discriminant)) / (2.0 * a);
-    return t;
+    // makes direction unit_vector -1.0 < y 1.0
+    vec3 unit_direction = unit_vector(r.direction());
+
+    // 0.0 < t < 1.0
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    // linear interpolation of color between white and blue
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
   }
-  
-}
-
-vec3 color(const ray& r) {
-  // sphere at -1 on z axis of radius 0.5
-  //      we get the point where the sphere.
-  float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-
-  if(t > 0.0) {
-    vec3 N = unit_vector(r.point_at_parameter(t) -  vec3(0, 0, -1));
-    return 0.5 * vec3(N.x()+1, N.y()+1, N.z()+1);
-  }
-
-  // makes direction unit_vector -1.0 < y 1.0
-  vec3 unit_direction = unit_vector(r.direction());
-
-  // 0.0 < t < 1.0
-  t = 0.5 * (unit_direction.y() + 1.0);
-  // linear interpolation of color between white and blue
-  return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -54,6 +32,16 @@ int main() {
   vec3 vertical(0.0, 2.0, 0.0);
   vec3 origin(0.0, 0.0, 0.0);
 
+  // creates the objects of the world
+  hitable *objects[2];
+
+  // creates two spheres
+  objects[0] = new sphere(vec3(0, 0, -1), 0.5);
+  objects[1] = new sphere(vec3(0, -100.5, -1), 100);
+
+  hitable *world = new hitable_list(objects, 2);
+
+
   // main loop
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
@@ -63,7 +51,9 @@ int main() {
 
       // creates ray
       ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-      vec3 col = color(r);
+      
+      vec3 point = r.point_at_parameter(2.0);
+      vec3 col = color(r, world);
 
       // gets color components
       int ir = int(255.99*col[0]);
